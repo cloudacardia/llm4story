a6b372d3718861214ae79f72cf0c395ee205afd1
stupidHIGH <1370215371@qq.com>
2023-05-16 11:39:39 +0800

    Update llm4story.py
    greedy search for whys and explanations

--- repo/llm4story.py
+++ repo/llm4story.py
@@ -75,6 +75,8 @@ def clean_split(text):
         line = line.replace("  "," ").strip()
         if line!="":
             sentences.append(line)
+    if 'an AI language model' in sentences[0]:
+        sentences = sentences[1:]
     return sentences
 
 def get_all_recommend(movie_data, queries,topk=4):
@@ -109,10 +111,10 @@ def get_all_recommend(movie_data, queries,topk=4):
 
     return all_recommendations
 
-def ask_why(story,new_info=[],plots=None,depth=0):
+def ask_why(story,new_info={},plots=None,depth=0,width=2,story_summary=None):
 
     def summarize(story):
-        prompt = 'Here is a story: "' + story +'"\n' + 'Please summarize the above story and only give me an organized list of sentences, each of which describes one plot.\n'
+        prompt = 'Here is a story: "' + story +'"\n' + 'please point out '+str(width)+' major unclarities in the story in an organized list.\n'
         story_summary = generate(prompt)
         return story_summary
 
@@ -121,26 +123,32 @@ def ask_why(story,new_info=[],plots=None,depth=0):
         plots = clean_split(story_summary)
 
     new_plots = []
-    for plot in plots:
-        prompt = 'Here is a story: \n' + story +'\n' + 'Except for pure coincidence, give me a list of ideas about why does this happen: \n' + plot
-        explanations = generate(prompt)
-        explanations = clean_split(explanations)
-        explanation_num = random.randint(0, len(explanations)-1)
-        new_plots.append(explanations[explanation_num]) # pick an explanation randomly
-        new_info.append((plot,explanations[explanation_num]))
+    for index,plot in enumerate(plots):
+        prompt_1 = 'Here is a story: \n' + story +'\n' + 'An unclarity is: \n'+ plot +'\n' + 'Except for pure coincidence and subject reasons, reveal me some implicit background knowledge within one or two sentencese to rationalize the story. The additional information should be short and imply the topic: '+ list2text(queries['subjects']) + "."
+        explanation = generate(prompt_1)
+        #explanations = clean_split(explanations)
+        #prompt_2 = 'Here is a story: \n' + story +'\n' + 'Here is a plot: \n'+plot + '\n'+ 'Here are a list of possible reasons about why the above plot is reasonable in the story: \n' + list2text(explanations,type='phrase') + '\n' + ' pretend to be a professional writer, please select the reason that is the closest to the '+ list2text(queries['subjects']) + ' and only output the index number without any explanation.'
+        #explanation_num = random.randint(0, len(explanations)-1)
+        #output = generate(prompt_2)
+        #explanation_num = int(re.sub("[/.,a-zA-Z]*","",output).strip()) - 1 # prompt 里explaination的序号 进行了+1
+        new_plots.append(explanation) # pick an explanation
+        if str(index) not in list(new_info.keys()):
+            new_info[str(index)] = [(plot,explanation)]
+        else:
+            new_info[str(index)].append((plot,explanation))
 
     if depth < 1:
         depth += 1
-        new_info = ask_why(story, new_info, new_plots, depth)
-        return new_info
-    return new_info
+        story_summary,new_info = ask_why(story, new_info, new_plots, depth,width, story_summary)
+        return story_summary,new_info
+    return story_summary,new_info
 
-def add_new_info(story,new_info):
+def add_new_info(story,picked_info):
     num_parts = 2
     new_story = []
-    reasons = [each[1] for each in new_info]
-    reasons = "\n".join(reasons)
-    prompt = 'Here is a story: \n' + story +'\n' +'Here is some additional information: \n'+ reasons + '\n'+ \
+    # reasons = [each[1] for each in new_info]
+    # reasons = "\n".join(reasons)
+    prompt = 'Here is a story: \n' + story +'\n' +'Here is some additional information: \n'+ picked_info + '\n'+ \
              'Please add the above information to the story and preserve other details by repeating the unchanged parts and only modifying the necessary paragraphs. As the story is too long, to make sure all the details are well covered, only describe the first half of the story and only tell the second half when I request.'
     messages = [{"role": "user", "content": prompt}]
     for part in range(num_parts):
@@ -153,13 +161,27 @@ def add_new_info(story,new_info):
         generation = response['choices'][0]['message']['content']
 
         new_story.append(generation)
-        question = 'Describe the second half of the story.' #分两步生成故事
+        question = 'Without further ado, start generating the second half of the story.' #分两步生成故事
         messages.append({"role": "assistant", "content": generation})
         messages.append({"role": "user", "content": question})
 
     new_story = "\n".join(new_story)
     return new_story
+def pick_info(story,new_info):
+    chain_of_reasons = []
+    for index in list(new_info.keys()):
+        chain_of_reason = []
+        for each in list(reversed(new_info[index])):
+            chain_of_reason.append(each[-1])
+        chain_of_reason.append(each[0])
+        chain_of_reason = list(reversed(chain_of_reason))
+        chain_of_reasons.append(". ".join(chain_of_reason).replace("..","."))
 
+    prompt = 'Here is a story: \n' + story + '\n' + 'Here are a list of reasons, explaining an unclarity in the story in depth: \n' + list2text(chain_of_reasons,type='phrase') + '\n' + 'Pretend to be a professional writer, please select the reason that is the closest to the '+ list2text(queries['subjects']) + ' and only output the index number without any explanation.'
+    reason_id = int(re.sub("[/.,a-zA-Z]*","",generate(prompt)).strip()) - 1
+    return chain_of_reasons[reason_id]
+
+    #return picked_info
 if __name__ == '__main__':
     queries = {'mood':'excited, thrilled','style':'magical','genre':['action'],'subjects':['adventure','death'],'plots':['he escaped from the island','he met a mermaid','he had a soup']}
 
@@ -186,11 +208,15 @@ if __name__ == '__main__':
     print("\n\n-----------------------------------------------\n\n")
 
 
-    new_info = ask_why(story)
+    story_summary,new_info = ask_why(story)
     print(new_info)
     print("\n\n-----------------------------------------------\n\n")
 
-    new_info = new_info[-3:-1]
-    new_story = add_new_info(story, new_info).replace("\n\n","\n")
+    picked_info = pick_info(story,new_info)
+    print(picked_info)
+    print("\n\n-----------------------------------------------\n\n")
+
+    #new_info = new_info[-3:-1]
+    new_story = add_new_info(story, picked_info).replace("\n\n","\n")
     print(new_story)
     print("\n\n-----------------------------------------------\n\n")
