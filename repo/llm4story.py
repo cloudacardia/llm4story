dc744b9d47075d7a2f5124fda641745bed56b673
cloudacardia <109971030+cloudacardia@users.noreply.github.com>
2023-05-23 00:10:07 +0800

    Update llm4story.py

--- repo/llm4story.py
+++ repo/llm4story.py
@@ -3,6 +3,7 @@ import json
 import random
 import os, re
 import torch
+import time
 from sentence_transformers import SentenceTransformer, util
 from utils import *
 
@@ -58,7 +59,7 @@ def single_corpus_recommend(entry_name,query,corpus,k=4):
 
     # Find the top-2 corpus documents matching each query
     hits = util.semantic_search(query_embeddings, corpus_embeddings, top_k=k)
-    #print(hits)
+    # print(hits)
     recommendation = [(i['corpus_id'],corpus[i['corpus_id']]) for i in hits[0]]
     return recommendation
 
@@ -102,10 +103,10 @@ def get_all_recommend(movie_data, queries,topk=4):
         all_subjects.append(subjects)
         all_plots.append(plots)
 
-    #all_recommendations['mood_recommend'] = single_corpus_recommend('mood',queries['mood'], all_moods, k=topk)
-    #all_recommendations['style_recommend'] = single_corpus_recommend('style',queries['style'], all_styles, k=topk)
+    # all_recommendations['mood_recommend'] = single_corpus_recommend('mood',queries['mood'], all_moods, k=topk)
+    # all_recommendations['style_recommend'] = single_corpus_recommend('style',queries['style'], all_styles, k=topk)
 
-    #all_recommendations['genre_recommend'] = multi_corpus_recommend('genre',queries['genre'], all_genres, k=topk)
+    # all_recommendations['genre_recommend'] = multi_corpus_recommend('genre',queries['genre'], all_genres, k=topk)
     all_recommendations['subject_recommend'] = multi_corpus_recommend('subjects',queries['subjects'], all_subjects, k=topk)
     all_recommendations['plot_recommend'] = multi_corpus_recommend('plots',queries['plots'], all_plots, k=topk)
 
@@ -126,11 +127,11 @@ def ask_why(story,new_info={},plots=None,depth=0,width=2,story_summary=None):
     for index,plot in enumerate(plots):
         prompt_1 = 'Here is a story: \n' + story +'\n' + 'An unclarity is: \n'+ plot +'\n' + 'Except for pure coincidence and subject reasons, reveal me some implicit background knowledge within one or two sentencese to rationalize the story. The additional information should be short and imply the topic: '+ list2text(queries['subjects']) + "."
         explanation = generate(prompt_1)
-        #explanations = clean_split(explanations)
-        #prompt_2 = 'Here is a story: \n' + story +'\n' + 'Here is a plot: \n'+plot + '\n'+ 'Here are a list of possible reasons about why the above plot is reasonable in the story: \n' + list2text(explanations,type='phrase') + '\n' + ' pretend to be a professional writer, please select the reason that is the closest to the '+ list2text(queries['subjects']) + ' and only output the index number without any explanation.'
-        #explanation_num = random.randint(0, len(explanations)-1)
-        #output = generate(prompt_2)
-        #explanation_num = int(re.sub("[/.,a-zA-Z]*","",output).strip()) - 1 # prompt 里explaination的序号 进行了+1
+        # explanations = clean_split(explanations)
+        # prompt_2 = 'Here is a story: \n' + story +'\n' + 'Here is a plot: \n'+plot + '\n'+ 'Here are a list of possible reasons about why the above plot is reasonable in the story: \n' + list2text(explanations,type='phrase') + '\n' + ' pretend to be a professional writer, please select the reason that is the closest to the '+ list2text(queries['subjects']) + ' and only output the index number without any explanation.'
+        # explanation_num = random.randint(0, len(explanations)-1)
+        # output = generate(prompt_2)
+        # explanation_num = int(re.sub("[/.,a-zA-Z]*","",output).strip()) - 1 # prompt 里explaination的序号 进行了+1
         new_plots.append(explanation) # pick an explanation
         if str(index) not in list(new_info.keys()):
             new_info[str(index)] = [(plot,explanation)]
@@ -161,7 +162,7 @@ def add_new_info(story,picked_info):
         generation = response['choices'][0]['message']['content']
 
         new_story.append(generation)
-        question = 'Without further ado, start generating the second half of the story.' #分两步生成故事
+        question = 'Without further ado, start generating the second half of the story.' # 分两步生成故事
         messages.append({"role": "assistant", "content": generation})
         messages.append({"role": "user", "content": question})
 
@@ -181,42 +182,117 @@ def pick_info(story,new_info):
     reason_id = int(re.sub("[/.,a-zA-Z]*","",generate(prompt)).strip()) - 1
     return chain_of_reasons[reason_id]
 
-    #return picked_info
+    # return picked_info
+
+plot_kind = []
+with open("test.prompt.txt", 'r', encoding='utf-8') as file:# yyx change
+    reddit_plot = [k.strip() for k in file.readlines()]
+    for i in range(len(reddit_plot)):
+        j = reddit_plot[i].find('[ ')
+        if j != -1:# kind:[_IP/WP/FF/EU/CW/RF/OT/PI/Wp/PM_]_
+            plot_kind.append(reddit_plot[i][j+2:j+4])
+            reddit_plot[i] = reddit_plot[i][0:j].strip() + reddit_plot[i][j+6:].strip()
+        else:
+            plot_kind.append('')
+
+
 if __name__ == '__main__':
-    queries = {'mood':'excited, thrilled','style':'magical','genre':['action'],'subjects':['adventure','death'],'plots':['he escaped from the island','he met a mermaid','he had a soup']}
+    with open('../data/movie_data.json', 'r') as fi:
+        movie_data = json.load(fi)
+    num = 0
 
-    with open('../data/movie_data.json', 'r') as f:
-        movie_data = json.load(f)
+    while num != len(plot_kind):# 反复续写文件
+        try:
+            outputFile = open("results.txt",'r',encoding = 'utf-8')
+            output_list = outputFile.readlines()
+            line_sub = len(output_list) - 1 - output_list[::-1].index("********************************************************************\n")
 
-    all_recommendations = get_all_recommend(movie_data, queries,4)
+            output_list = output_list[0:line_sub+1]# 保存生成好的文本
+            outputFile.close()
+            output_file = open("results.txt", 'w', encoding='utf-8')
+            num = 0# 已经生成的plot个数
+            for line in output_list:
+                output_file.write(line)
+                if line == "********************************************************************\n":
+                    num += 1
+            output_file.write("\n")
+            print(num)
+        except:# 刚开始写文件
+            num=0
+            output_file = open("results.txt", 'w', encoding='utf-8')
 
-    example_id_1 = all_recommendations['plot_recommend'][0][0]
-    example_id_2 = all_recommendations['plot_recommend'][1][0]
+        for simple_plot in reddit_plot[num:]:
+            flag = 0
+            start = time.time()
+            for i in range(20):# 不成功则尝试足够次数?——次数是否合适
+                if flag == 1:
+                    # save
+                    output_file.write("plot:" + simple_plot)
+                    output_file.write("\n")
+                    output_file.write(prompt)
+                    output_file.write("\n\n-----------------------------------------------\n\n")
+                    output_file.write(story)
+                    output_file.write("\n\n-----------------------------------------------\n\n")
+                    output_file.write(str(new_info))
+                    output_file.write("\n\n-----------------------------------------------\n\n")
+                    output_file.write(str(picked_info))
+                    output_file.write("\n\n-----------------------------------------------\n\n")
+                    output_file.write(new_story)
+                    output_file.write("\n\n-----------------------------------------------\n\n")
+                    output_file.write("********************************************************************\n")
+                    break
+                else:
+                    try:
+                        # print("plot:"+simple_plot)
+                        queries = {'mood':'excited, thrilled','style':'magical','genre':['action'],'subjects':['adventure','death']}
+                        queries['plots'] = [simple_plot]
+                        # queries = {'mood':'excited, thrilled','style':'magical','genre':['action'],'subjects':['adventure','death'],
+                        # 'plots':['he escaped from the island','he met a mermaid','he had a soup']}
 
-    example_1 = movie_data[example_id_1]
-    example_2 = movie_data[example_id_2]
-    examples = [example_1]
-    #examples = []
-    #examples = [example_1, example_2]
+                        all_recommendations = get_all_recommend(movie_data, queries,4)
 
-    prompt = make_prompt(examples=examples, conditions=queries)
-    print(prompt)
-    print("\n\n-----------------------------------------------\n\n")
+                        example_id_1 = all_recommendations['plot_recommend'][0][0]
+                        example_id_2 = all_recommendations['plot_recommend'][1][0]
 
-    story = generate(prompt).replace("\n\n","\n")
-    print(story)
-    print("\n\n-----------------------------------------------\n\n")
+                        example_1 = movie_data[example_id_1]
+                        example_2 = movie_data[example_id_2]
+                        examples = [example_1]
+                        # examples = []
+                        # examples = [example_1, example_2]
+
+                        prompt = make_prompt(examples=examples, conditions=queries)
+                        # print(prompt)
+                        # print("\n\n-----------------------------------------------\n\n")
+
+                        story = generate(prompt).replace("\n\n","\n")
+                        # print(story)
+                        # print("\n\n-----------------------------------------------\n\n")
 
 
-    story_summary,new_info = ask_why(story)
-    print(new_info)
-    print("\n\n-----------------------------------------------\n\n")
+                        story_summary,new_info = ask_why(story)
+                        # print(new_info)
+                        # print("\n\n-----------------------------------------------\n\n")
 
-    picked_info = pick_info(story,new_info)
-    print(picked_info)
-    print("\n\n-----------------------------------------------\n\n")
+                        picked_info = pick_info(story,new_info)
+                        # print(picked_info)
+                        # print("\n\n-----------------------------------------------\n\n")
 
-    #new_info = new_info[-3:-1]
-    new_story = add_new_info(story, picked_info).replace("\n\n","\n")
-    print(new_story)
-    print("\n\n-----------------------------------------------\n\n")
+                        # new_info = new_info[-3:-1]
+                        new_story = add_new_info(story, picked_info).replace("\n\n","\n")
+
+                        print("plot:"+simple_plot+"\n")
+                        # print(new_story)
+                        # print("\n\n-----------------------------------------------\n\n")
+                        print("********************************************************************")
+
+                        flag = 1
+                    except:
+                        time.sleep(3)# 出错时自动等3秒,继续发送任务# 时间是否合适
+                        flag = 0
+                        if time.time() - start>120.0:# 若时间超过两分钟且未出结果，则重新续写文件
+                            break
+            if time.time() - start > 120.0:  # 若时间超过两钟且未出结果，则重新续写文件
+                break
+            if i==19:# 尝试了10次时 可以认为失败
+                print("error!")
+        output_file.close()
